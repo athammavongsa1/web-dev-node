7d6e135fe749a56e5468564fc1f97c9887ae43ed
Alexadriana Thammavongsa <Alex@AlexadrianasAir.attlocal.net>
2021-11-18 23:48:45 -0600

    .

--- services/profile-service.js
+++ services/profile-service.js
@@ -5,4 +5,19 @@ module.exports = (app) => {
     res.json(profile);
   };
   app.get("/api/profile/", getCurrentProfile);
+
+  const updateCurrentProfile = (req, res) => {
+    profile = {
+      ...profile,
+      website: req.body.website_,
+      bio: req.body.bio_,
+      location: req.body.location_,
+      name: req.body.name_,
+      handle: req.body.hand_,
+      dateOfBirth: req.body.bday_,
+    };
+    res.json(profile);
+    res.setStatus(200);
+  };
+  app.put("/api/profile", updateCurrentProfile);
 };
