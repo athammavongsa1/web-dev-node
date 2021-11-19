41c82c818e44026be7bcb6eeeb1968d662d635e6
Alexadriana Thammavongsa <Alex@AlexadrianasAir.attlocal.net>
2021-11-18 20:56:16 -0600

    .

--- services/profile-service.js
+++ services/profile-service.js
@@ -1,8 +1,8 @@
 let profile = require("../data/profile.json");
 
 module.exports = (app) => {
-  const getProfile = (req, res) => {
+  const getCurrentProfile = (req, res) => {
     res.json(profile);
   };
-  app.get("/api/profile/", getProfile);
+  app.get("/api/profile/", getCurrentProfile);
 };
